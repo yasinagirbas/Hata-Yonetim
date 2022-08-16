cfef5b827eeae6eca7a659db8fd7ebc7237ecbdf
yasinagirbas <98206352+yasinagirbas@users.noreply.github.com>
2022-08-16 16:03:11 +0300

    newbranch

--- Hata-Yonetimi/Program.cs
+++ Hata-Yonetimi/Program.cs
@@ -15,11 +15,32 @@
             {
                 Console.WriteLine("Hata : "+ex.Message.ToString());
             }
-            finally
-            {
-                Console.WriteLine("işlem tamamlandı.");
-            }
-            
+            //finally
+            //{
+             //   Console.WriteLine("işlem tamamlandı.");
+           // }
+
+           try
+           {
+               //int a = int.Parse(null); // Burada yapılacak işlem consol ekranda boş geçemeyeceğine 
+               int a = int.Parse("-200000000");
+           }
+           catch (ArgumentNullException ex) //
+           {
+               Console.WriteLine("boş değer girdiniz");
+               Console.WriteLine(ex);
+           }
+           catch (FormatException ex)
+           {
+               Console.WriteLine("veri tipi uygun değil");
+               Console.WriteLine(ex);
+           }
+           catch (OverflowException ex)
+           {
+               Console.WriteLine("çok küçük yada çok büyük değer girdiniz");
+               Console.WriteLine(ex);
+               
+           }
         }
         
         
