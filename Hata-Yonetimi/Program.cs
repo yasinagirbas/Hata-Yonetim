6f11ecf408cd9dda0760b80f10942db73bf061d7
yasinagirbas <yasin.agirbas@hotmail.com>
2022-08-16 15:48:42 +0300

    First

---
+++ Hata-Yonetimi/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Hata_Yonetim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("bir sayi giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi : " + sayi);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata : "+ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı.");
+            }
+            
+        }
+        
+        
+        
+    }
+    
+}
+
